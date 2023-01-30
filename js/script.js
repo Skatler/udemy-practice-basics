1c6453dbe482e1ca7d8a842ac10550ef72ad82ba
Skatler <yaroslavzozuliya95@gmail.com>
2023-01-30 16:00:51 +0200

    v2

--- js/script.js
+++ js/script.js
@@ -1,18 +1,4 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
-'use strict';
+"use strict";
 
 const movieDB = {
     movies: [
@@ -24,3 +10,22 @@ const movieDB = {
     ]
 };
 
+const blockAdvertisement = document.querySelector(".promo__advertisement");
+blockAdvertisement.remove();
+
+const promoTitle = document.querySelector(".promo__genre");
+promoTitle.textContent = "ДРАМА";
+
+const promoBg = document.querySelector(".promo__bg");
+promoBg.style.backgroundImage = 'url("img/bg.jpg")';
+
+const movieList = document.querySelector(".promo__interactive-list");
+movieList.innerHTML = "";
+
+movieDB.movies.sort();
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+    <li class="promo__interactive-item">${i + 1} ${film}
+        <div class="delete"></div>
+    </li>`;
+});
