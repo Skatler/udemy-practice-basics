d122899058b4d109f64413c487861824424709e8
Skatler <yaroslavzozuliya95@gmail.com>
2023-01-31 10:09:26 +0200

    v3

--- js/script.js
+++ js/script.js
@@ -1,30 +1,28 @@
-"use strict";
-
 const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
+  movies: [
+    'Логан',
+    'Лига справедливости',
+    'Ла-ла лэнд',
+    'Одержимость',
+    'Скотт Пилигрим против...',
+  ],
 };
 
-const blockAdvertisement = document.querySelector(".promo__advertisement");
+const blockAdvertisement = document.querySelector('.promo__advertisement');
 blockAdvertisement.remove();
 
-const promoTitle = document.querySelector(".promo__genre");
-promoTitle.textContent = "ДРАМА";
+const promoTitle = document.querySelector('.promo__genre');
+promoTitle.textContent = 'ДРАМА';
 
-const promoBg = document.querySelector(".promo__bg");
+const promoBg = document.querySelector('.promo__bg');
 promoBg.style.backgroundImage = 'url("img/bg.jpg")';
 
-const movieList = document.querySelector(".promo__interactive-list");
-movieList.innerHTML = "";
+const movieList = document.querySelector('.promo__interactive-list');
+movieList.innerHTML = '';
 
 movieDB.movies.sort();
 movieDB.movies.forEach((film, i) => {
-    movieList.innerHTML += `
+  movieList.innerHTML += `
     <li class="promo__interactive-item">${i + 1} ${film}
         <div class="delete"></div>
     </li>`;
